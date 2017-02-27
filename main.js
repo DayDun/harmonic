f0b22ae41460dc25e700e2a3679ff0e8f7999f9d
TheBlu <tanel58@hotmail.com>
2017-02-27 19:06:50 +0200

    initial Tone.js support

--- main.js
+++ main.js
@@ -497,71 +497,82 @@ harmonic.init = function() {
                      window.oAudioContext   ||
                      window.msAudioContext;
   if (audioContext) {
-    // Init audio modules
-    harmonic.audioContext = new audioContext();
     
-    // Audio Analyser for oscilloscope
-    harmonic.audioAnalyser = harmonic.audioContext.createAnalyser();
-    harmonic.audioAnalyser.connect(harmonic.audioContext.destination);
-    harmonic.audioAnalyser.fftSize = 1024;
-    harmonic.audioAnalyser.smoothingTimeConstant = 1;
-    harmonic.audioAnalyser.dataArray = new Uint8Array(harmonic.audioAnalyser.frequencyBinCount);
-    harmonic.audioAnalyser.render = function() {
+    // set up mixer
+    harmonic.mixer = function() {
+      this.master = new Tone.Signal();
+      this.master.receive("master");
+      var comp = new Tone.Compressor();
+      this.master.connect(comp);
+      var delay = new Tone.FeedbackDelay(0.3, 0.2);
+      var delayGain =  new Tone.Gain(0.3);
+      comp.connect(delay);
+      delay.connect(delayGain);
+      var reverb = new Tone.Freeverb(0.6, 6000);
+      var reverbGain = new Tone.Gain(0.1);
+      this.master.connect(reverb);
+      delayGain.connect(reverb);
+      reverb.connect(reverbGain);
+      var masterGain = new Tone.Gain();
+      this.master.connect(masterGain);
+      delayGain.connect(masterGain);
+      reverbGain.connect(masterGain);
+      this.analyser = new Tone.Analyser("waveform");
+      masterGain.connect(analyser).toMaster();
+
+      return this;
+    }();
+    harmonic.render = function() {
       var ctx = document.getElementById("oscilloscope").getContext("2d");
       ctx.clearRect(0, 0, 150, 40);
-      harmonic.audioAnalyser.getByteTimeDomainData(harmonic.audioAnalyser.dataArray);
+
+      var wave = harmonic.mixer.analyser.analyse();
       
       ctx.lineWidth = 1;
       ctx.strokeStyle = "#dde5e9";
       ctx.beginPath();
       
       var x = 0;
-      ctx.moveTo(0, harmonic.audioAnalyser.dataArray[0] / 128 * 20);
-      harmonic.audioAnalyser.dataArray.forEach(function(data) {
+      ctx.moveTo(0, wave[0] / 128 * 20);
+      wave.forEach(function(data) {
         ctx.lineTo(x, data / 128 * 20);
         
-        x += 150 / harmonic.audioAnalyser.dataArray.length;
+        x += 150 / wave.length;
       });
       ctx.stroke();
       
-      window.requestAnimationFrame(harmonic.audioAnalyser.render);
+      window.requestAnimationFrame(harmonic.render);
     };
-    harmonic.audioAnalyser.render();
-    
-    harmonic.masterCompress = harmonic.audioContext.createDynamicsCompressor();
-    harmonic.masterCompress.connect(harmonic.audioAnalyser);
-    
-    harmonic.masterVolume = harmonic.audioContext.createGain();
-    harmonic.masterVolume.connect(harmonic.masterCompress);
-    
+    harmonic.render();
     
     // Create default instruments
     instrument.create("osc", {
-      name: "Basic Oscillator",
+      name: "3xOsc",
       initialized: false,
       keys: [],
       options: {
-        osc1: {
-          type: "sawtooth",
-          coarse: 0
-        },
-        osc2: {
-          type: "sawtooth",
-          volume: 1,
-          coarse: -12
-        },
-        osc3: {
-          type: "square",
-          volume: 1,
-          coarse: 0
-        }
+        oscs: [
+          {
+            type: "sawtooth",
+            pitchBend: 0
+          },
+          {
+            type: "sawtooth",
+            volume: 1,
+            pitchBend: -12
+          },
+          {
+            type: "square",
+            volume: 1,
+            pitchBend: 0
+          }
+        ]
       },
-      init: function(audioOutput) {
-        this.output = audioOutput;
+      init: function() {
+        this.output = harmonic.mixer.master;
 
         this.initialized = true;
         
-        // Return gui object
         this.gui = {
           instrument: this,
           width: 300,
@@ -595,7 +606,7 @@ harmonic.init = function() {
                   ],
                   value: "sawtooth",
                   oninput: function(value, instrument) {
-                    instrument.options.osc1.type = value;
+                    instrument.options.oscs[0].type = value;
                   }
                 },
                 {
@@ -606,7 +617,7 @@ harmonic.init = function() {
                   max: 120,
                   value: 0,
                   oninput: function(value, instrument) {
-                    instrument.options.osc1.coarse = value;
+                    instrument.options.oscs[0].pitchBend = value;
                   }
                 }
               ]
@@ -639,7 +650,7 @@ harmonic.init = function() {
                   ],
                   value: "sawtooth",
                   oninput: function(value, instrument) {
-                    instrument.options.osc2.type = value;
+                    instrument.options.oscs[1].type = value;
                   }
                 },
                 {
@@ -650,7 +661,7 @@ harmonic.init = function() {
                   max: 120,
                   value: -12,
                   oninput: function(value, instrument) {
-                    instrument.options.osc2.coarse = value;
+                    instrument.options.oscs[1].pitchBend = value;
                   }
                 }
               ]
@@ -683,7 +694,7 @@ harmonic.init = function() {
                   ],
                   value: "square",
                   oninput: function(value, instrument) {
-                    instrument.options.osc3.type = value;
+                    instrument.options.oscs[2].type = value;
                   }
                 },
                 {
@@ -694,50 +705,35 @@ harmonic.init = function() {
                   max: 120,
                   value: 0,
                   oninput: function(value, instrument) {
-                    instrument.options.osc3.coarse = value;
+                    instrument.options.oscs[2].pitchBend = value;
                   }
                 }
               ]
             }
           ]
         };
+
+
+
+        this.oscs = [];
+        for (var i = 0; i < 3; i++) {
+          var osc = new Tone.Synth();
+          osc.connect(this.output);
+          osc.options = this.options.oscs[i];
+          this.oscs.push(osc);
+        }
       },
       noteOn: function(pitch, velocity) {
-        return this.keys.push({
-          pitch: pitch,
-          velocity: velocity,
-          init: function(output, options) {
-            this.osc1 = harmonic.audioContext.createOscillator();
-            this.osc1.frequency.value = harmonic.getFrequencyByPitch(pitch + options.osc1.coarse);
-            this.osc1.type = options.osc1.type;
-            this.osc1.connect(output);
-            this.osc1.start();
-            
-            this.osc2 = harmonic.audioContext.createOscillator();
-            this.osc2.frequency.value = harmonic.getFrequencyByPitch(pitch + options.osc2.coarse);
-            this.osc2.type = options.osc2.type;
-            this.osc2.connect(output);
-            this.osc2.start();
-            
-            this.osc3 = harmonic.audioContext.createOscillator();
-            this.osc3.frequency.value = harmonic.getFrequencyByPitch(pitch + options.osc3.coarse);
-            this.osc3.type = options.osc3.type;
-            this.osc3.connect(output);
-            this.osc3.start();
-            return this;
-          },
-          kill: function() {
-            this.osc1.stop();
-            this.osc2.stop();
-            this.osc3.stop();
-          }
-        }.init(this.output, this.options)) - 1;
+        for (var i = 0; i < 3; i++) {
+          var osc = this.oscs[i];
+          osc.triggerAttack(harmonic.getFrequencyByPitch(pitch + osc.options.pitchBend));
+        }
+        return {pitch, velocity};
       },
       noteOff: function(id, velocity) {
-        if (this.keys[id]) {
-          this.keys[id].kill();
-          
-          delete this.keys[id];
+        for (var i = 0; i < 3; i++) {
+          var osc = this.oscs[i];
+          osc.triggerRelease();
         }
       },
       tick: function() {}
@@ -874,6 +870,50 @@ harmonic.init = function() {
             
           }
         }.init(this.output, this.player, this.options)) - 1;
+      },
+      noteOff: function(id, velocity) {
+        if (this.keys[id]) {
+          this.keys[id].kill();
+          
+          delete this.keys[id];
+        }
+      },
+      tick: function() {}
+    });
+
+    instrument.create("tone", {
+      name: "Tone.js",
+      initialized: false,
+      keys: [],
+      init: function(audioOutput) {
+        this.output = audioOutput;
+
+        this.initialized = true;
+        
+        // Return gui object
+        this.gui = {
+          children: []
+        };
+      },
+      noteOn: function(pitch, velocity) {
+        return this.keys.push({
+          pitch: pitch,
+          velocity: velocity,
+          init: function(output, options) {
+            this.tone = new Tone.Synth();
+            this.tone.send("master");
+            this.tone.triggerAttackRelease(harmonic.getFrequencyByPitch(pitch, '8n'));
+
+            // declick pt1
+            this.tone.volume.rampTo(0, 0.05);
+            
+            return this;
+          },
+          kill: function() {
+            // declick pt2
+            this.tone.volume.rampTo(-Infinity, 0.5);
+          }
+        }.init(this.output)) - 1;
       },
       noteOff: function(id, velocity) {
         if (this.keys[id]) {
